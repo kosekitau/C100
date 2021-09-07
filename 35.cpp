edbcd4acf117b5032ed8b73dd23e093b88034428
koseki <kosekitau@gmail.com>
2021-09-07 09:18:08 +0900

    36

--- 35.cpp
+++ 35.cpp
@@ -15,13 +15,23 @@ using P = pair<int, int>;
 #define MOD 1000000007
 
 int main(){
-    ll n;
-    cin>>n;
-    ll fib[n+1];
-    fib[0]=1; fib[1]=1;
+    int n, w;
+    cin>>n>>w;
+    int V[n], W[n], dp[n+1][w+1];
+    for(int i=0;i<n;i++) cin>>V[i]>>W[i];
+    for(int i=0;i<=w;i++) dp[0][i]=0;
 
-    for(int i=2;i<n+1;i++) fib[i]=fib[i-1]+fib[i-2];
-
-    cout<<fib[n]<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=w;j++){
+            if(j>=W[i]){
+                dp[i+1][j]=max( dp[i][j], dp[i][j-W[i]]+V[i] );
+            }
+            else{
+                dp[i+1][j]=dp[i][j];
+            }
+        }
+    }
+    
+    cout<<dp[n][w]<<endl;
     return 0;
 }
--- 36.cpp
+++ 36.cpp
@@ -17,21 +17,23 @@ using P = pair<int, int>;
 int main(){
     int n, w;
     cin>>n>>w;
-    int V[n], W[n], dp[n+1][w+1];
+    int V[n], W[n];
     for(int i=0;i<n;i++) cin>>V[i]>>W[i];
+
+    int dp[n+1][w+1];
     for(int i=0;i<=w;i++) dp[0][i]=0;
 
     for(int i=0;i<n;i++){
         for(int j=0;j<=w;j++){
             if(j>=W[i]){
-                dp[i+1][j]=max( dp[i][j], dp[i][j-W[i]]+V[i] );
+                dp[i+1][j]=max(dp[i][j], dp[i+1][j-W[i]]+V[i]);
             }
             else{
                 dp[i+1][j]=dp[i][j];
             }
         }
     }
-    
+
     cout<<dp[n][w]<<endl;
     return 0;
 }
