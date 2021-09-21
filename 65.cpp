18f5264dd53d69e1344cd99d0e59ca15a3b75ec8
koseki <kosekitau@gmail.com>
2021-09-21 21:40:16 +0900

    65

---
+++ 65.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+#define MAX_N 10000000
+
+struct edge{int u, v, cost;};
+
+int par[MAX_N];
+int depth[MAX_N];
+edge es[MAX_N];
+int V, E, K;
+
+void init(int n){
+    for(int i=0;i<n;i++){
+        par[i]=i;
+        depth[i]=0;
+    }
+}
+
+int find(int x){
+    if(par[x]==x){
+        return x;
+    }
+    else{
+        return par[x]=find(par[x]);
+    }
+}
+
+void unite(int x, int y){
+    x=find(x);//それぞれの親を見つける
+    y=find(y);
+    if(x==y) return;
+    if(depth[x]<depth[y]){
+        par[x]=y;
+    }else{
+        par[y]=x;
+        if(depth[x]==depth[y]) depth[x]++;
+    }
+}
+
+bool same(int x, int y){
+    return find(x)==find(y);
+}
+
+bool comp(const edge& e1, const edge& e2){
+    return e1.cost < e2.cost;
+}
+
+int kruskal(){
+    sort(es, es+E, comp);
+    init(V);
+    int res=0;
+    int cnt=0;
+    for(int i=0;i<E;i++){
+        if(cnt==V-K) break;
+        edge e = es[i];
+        if(!same(e.u, e.v)){
+            unite(e.u, e.v);
+            res+=e.cost;
+            cnt++;
+        }
+    }
+    return res;
+}
+
+int main(){
+    cin>>V>>E>>K;
+    for(int i=0;i<E;i++){
+        edge e;
+        cin>>e.u>>e.v>>e.cost;
+        es[i]=e;
+    }
+    cout<<kruskal()<<endl;
+    
+    return 0;
+}
