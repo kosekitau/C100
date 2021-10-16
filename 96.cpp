674ccafc7555ac3e5459316483563b5db50643cb
koseki <kosekitau@gmail.com>
2021-10-16 15:37:54 +0900

    96

---
+++ 96.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <set>
+#include <tuple>
+#include <map>
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int> >;
+using P = pair<int, int>;
+#define INF 2000000003
+#define MOD 998244353
+#define MAX_N 3001
+
+int main(){
+    ll n;
+    cin>>n;
+    cout<<(n-1)*n/2<<endl;
+    return 0;
+}
